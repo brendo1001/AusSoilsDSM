14e01225a52217111fd769eb4b291a5d5cb3e64b
zundp <peter.zund@qld.gov.au>
2021-04-08 11:08:18 +1000

    Version 1.3
    Added concordance and bias stats for model fit for both model calibration and validation

--- SLGA/Development/AvailP/Ranger_implementation.R
+++ SLGA/Development/AvailP/Ranger_implementation.R
@@ -1,25 +1,28 @@
 # Random Forest modeling for digital soil mapping
-# Using Ranger within Caret implementation of the orginal Random Forests algorithium 
+# Using Ranger within Caret implementation of the original Random Forests algorithm 
 # Using Ranger for speed and Caret so that the predict raster function works
 # Random hold back of 30% of training data for validation of model
 # Model fitting using 10 K-folds with cross validation, final model fitted to 70% of training data
-# Uncertainity anaylsis by Bootstrapping
+# Uncertainty analysis by Bootstrapping
 #
-# Version 0.1 - 22/05/2020 - Orginal script
-# Version 1.0 - 8/10/2020 - Bug with fitControl parameters (final model created using training data only)
+# Version 1.3 - 17/03/2021 - Added concordance and bias stats of model fit for both model calibration and validation
+# Version 1.2 - 15/03/2021 - Added variable importance calculation
+# Version 1.1 - 28/01/2021 - Fixed bug with scripts ability to handle variable number of depths (not just the standard GSM depths) in the Training data file.
+# Version 1.0 - 08/10/2020 - Bug with fitControl parameters (final model created using training data only)
+# Version 0.1 - 22/05/2020 - Original script
 #
 # Required data and format
 # Training data in csv file in TrainingData subdirectory with following column label and order: X, Y, ID, VALUE
-# Covariate data in GTiff format, mosaic of whole modelling area in Covariate/Mosaics subdirectory
+# Covariate data in GTiff format, mosaic of whole modeling area in Covariate/Mosaics subdirectory
 # Covariate tiles in Covariate/Tiles/TitleNumber subdirectory
 #
 ## User inputs
-wd <- "E://Temp//TERN2" #set working directory
+wd <- "E:\\LASER5" #set working directory
 
 ## Processing starts here #####
 ###library(httr)
 library(raster)
-###library(rgdal)
+library(rgdal)
 library(sp)
 ###library(devtools)
 
@@ -27,12 +30,13 @@ library(sp)
 # Get training data
 td <- paste(wd, "//TrainingData", sep = "")
 setwd(td)
-data <- read.table("PData.csv", header = TRUE, sep = ",")
+data <- read.table("TP_Training_Data.csv", header = TRUE, sep = ",")
 depths <- names(data[-(1:3)]) # List of depths in training data
 
 # Get Covariates for whole modelling area
-files <- list.files(path = paste(wd, "//Covariates//Mosaics", sep = ""), pattern = "\\.tif", full.names = TRUE) 
-###files <- list.files(path = "M://Projects/PMap/Modelling/Stage1/P2/Cubist/Covariates", pattern = "\\.tif$", full.names = TRUE)
+##files <- list.files(path = paste(wd, "//Covariates//Mosaics", sep = ""), pattern = "\\.tif", full.names = TRUE) 
+files <- list.files(path = paste(wd, "//Covariates", sep = ""), pattern = "\\.tif", full.names = TRUE) 
+
 
 # Stack covariate rasters
 r1 <- raster(files[1])
@@ -58,13 +62,25 @@ library(gstat)
 
 dir.create("Bootstrap") # Create Bootstrap models directory
 dir.create("Bootstrap/models")
-GOOFDat <- data.frame(Depths = NA, RMSE = NA, R2 = NA, MAE = NA, MSE = NA) # Summary stats df
+calGOOFDat <- data.frame(Depths = NA, Calib_RMSE = NA, Calib_R2 = NA, Calib_MAE = NA, Calib_MSE = NA, Calib_CC = NA, Calib_Bias = NA) #Calibration data stats summary df
+GOOFDat <- data.frame(Depths = NA, RMSE = NA, R2 = NA, MAE = NA, MSE = NA, CC = NA, Bias = NA) # Summary stats df
+ovi <- data.frame(Depth = NA) # Variable importance across depths df
+
+#Create concordance Function (M. Webb 2015)
+ccc<- function(observed, predicted){
+  mx=mean(observed)
+  my=mean(predicted)
+  s2x=var(observed)
+  s2y=var(predicted)
+  sxy=mean((observed-mx)*(predicted-my))
+  ccc=2*sxy/(s2x+s2y+(mx-my)^2 )
+  return(ccc)}
 
 ## For each depth fit a model
 # Establish calibration and validation data sets
 for (d in 1:length(depths)) {
   set.seed(123)
-  DSM_data_depth <- DSM_data[,c(1:3,3+d,10:(9+length(files)))] # Subset mod data to specfic depth
+  DSM_data_depth <- DSM_data[,c(1:3,3+d,(4+length(depths)):(3+length(depths)+length(files)))] # Subset mod data to specfic depth
   DSM_data_depth <- DSM_data_depth[complete.cases(DSM_data_depth),] 
   training <- sample(nrow(DSM_data_depth), 0.7 * nrow(DSM_data_depth)) # Vector with training data row numbers
   cDat <- DSM_data_depth[training, 4:(4+length(files))] # Based on number of covariates in ..//mosaic folder 
@@ -82,7 +98,7 @@ for (d in 1:length(depths)) {
   # Fit Ranger (RF) model for each bootstrap
   for (i in 1:nbag) {
     trainingREP <- sample.int(nrow(cDat), 1.0 * nrow(cDat), replace = TRUE)
-    fit_ranger <- train(VALUE ~ ., data = TrainDat, method = "ranger", trControl = fitControl)
+    fit_ranger <- train(VALUE ~ ., data = TrainDat, method = "ranger", trControl = fitControl, importance = 'impurity')
     modelFile <- paste(paste(paste(paste(getwd(), "/Bootstrap/models/", depths[d], "/", sep = ""), "bootMod_", sep = ""), i, sep = ""), ".rds", sep = "")
     saveRDS(object = fit_ranger, file = modelFile)
     }
@@ -90,9 +106,27 @@ for (d in 1:length(depths)) {
   # List all files indirectory
   r.models <- list.files(path = paste(getwd(), "/Bootstrap/models/", depths[d], "/", sep = ""), pattern = "\\.rds$", full.names = TRUE)
 
+  # Determine overall variable importance
+  fit_ranger <- readRDS(r.models[1])
+  imp <- varImp(fit_ranger)
+  vi <- imp$importance
+  for (i in 2:nbag) {
+    fit_ranger <- readRDS(r.models[i])
+    imp <- varImp(fit_ranger)
+    impdf <- imp$importance
+    vi <- cbind(vi, impdf)
+    }
+  vi$Mean <- rowMeans(vi) # Calculate mean across all bootstrap models
+  library(matrixStats) # Required for rowSds
+  vi$SD <- rowSds(as.matrix(vi[,1:nbag])) # Calculate standard deviation across all bootstrap models
+  vi$Rank <- (vi$Mean/vi$SD) # Calculate Rank based on mean/sd
+  vi <- vi[order(-vi$Rank),] #Order according to rank
+  mean <-  subset(vi, select = c('Mean'))
+  ovi <- cbind(ovi,mean)
+  
   # Assess goodness of fit
   # Calibration data
-  Mat <- matrix(NA, nrow = nbag, ncol = 4)
+  Mat <- matrix(NA, nrow = nbag, ncol = 6)
   for (i in 1:nbag) {
     fit_ranger <- readRDS(r.models[i])
     Dat <- cDat[1]
@@ -100,15 +134,19 @@ for (d in 1:length(depths)) {
     Dat$pred <- predict(fit_ranger, newdata = cDat)
     Mat[i, 1:3] <- as.matrix(defaultSummary(Dat)) # RMSE, R^2, MAE
     Mat[i, 4] <- mean((Dat$obs -Dat$pred)^2) #MSE
+    Mat[i, 5] <- ccc(Dat$obs, Dat$pred) # Concordance
+    Mat[i, 6] <- sum(Dat$pred - Dat$obs)/length(Dat$obs) # Bias
     }
 
   calDat <- as.data.frame(Mat)
-  names(calDat) <- c("RMSE", "R2", "MAE", "MSE")
-  colMeans(calDat)
-
+  names(calDat) <- c("Calib_RMSE", "Calib_R2", "Calib_MAE", "Calib_MSE", "Calib_CC", "Calib_Bias")
+  calDatmeans <- colMeans(calDat)
+  calDatmeans$Depths <- depths[d]
+  calGOOFDat <- rbind(calGOOFDat, calDatmeans)
+  
   #Validation data
   Pred.V <- matrix(NA, ncol = nbag, nrow = nrow(vDat))
-  cubiMat <- matrix(NA, nrow = nbag, ncol =4)
+  cubiMat <- matrix(NA, nrow = nbag, ncol =6)
   for (i in 1:nbag) {
     fit_ranger <- readRDS(r.models[i])
     Pred.V[, i] <- predict(fit_ranger, newdata = vDat)
@@ -117,14 +155,17 @@ for (d in 1:length(depths)) {
     Dat$pred <- predict(fit_ranger, newdata = vDat)
     cubiMat[i, 1:3] <- as.matrix(defaultSummary(Dat)) # RMSE, R^2, MAE
     cubiMat[i, 4] <- as.matrix(mean((Dat$obs - Dat$pred)^2)) #MSE
-    }
+    cubiMat[i, 5] <- ccc(Dat$obs, Dat$pred) # Concordance
+    cubiMat[i, 6] <- sum(Dat$pred - Dat$obs)/length(Dat$obs) # Bias
+  }
+  
   Pred.V_mean <- rowMeans(Pred.V)
 
   valDat <- as.data.frame(cubiMat)
-  names(valDat) <- c("RMSE", "R2", "MAE", "MSE")
+  names(valDat) <- c("RMSE", "R2", "MAE", "MSE", "CC", "Bias")
   valDatmeans <- colMeans(valDat)
   valDatmeans$Depths <- depths[d]
-  GOOFDat <- rbind(GOOFDat, valDatmeans)
+  GOOFDat <- rbind(GOOFDat, valDatmeans) # Validation stats
   
   avgMSE <- mean(valDat[, 4]) #Average validation MSE
   
@@ -169,10 +210,21 @@ for (d in 1:length(depths)) {
   dev.off()
 }
 
+# Summaries variable importance data
+ovi <- ovi[2:7]
+ovi <- setNames(ovi, c(depths[1],depths[2],depths[3],depths[4],depths[5],depths[6]))
+ovi$Mean <- rowMeans(ovi) # Calculate mean across all depths
+ovi$SD <- rowSds(as.matrix(ovi[,1:length(depths)])) # Calculate standard deviation across all bootstrap models
+ovi$Rank <- (ovi$Mean/ovi$SD) # Calculate Rank based on mean/sd
+ovi <- ovi[order(-ovi$Rank),] # Sort in descending order of importance
+
+GOOFDat <- cbind(calGOOFDat, GOOFDat[2:7]) # Combine calibration and validation stats
+
 # Write Goodness of Fit data to file
 setwd(paste(wd, "/Bootstrap/", sep = ""))
 write.csv(GOOFDat, "GOOFData.csv", row.names = FALSE)
 write.csv(avgMSE, "avgMSE.csv", row.names = FALSE) #Save to file for using in mapping part on HPC
+write.csv(ovi, "Overall_variable_importance.csv")
 
 ### Mapping
 ## Predict fitted model tile by tile
